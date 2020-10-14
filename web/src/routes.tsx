ef54adfea88dbbb01544534f611785bb6b701f70
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-10-14 09:18:41 -0300

    Setting up the Landing Page and Starting routes

---
+++ web/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Route path="/" component>
+
+            </Route>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
