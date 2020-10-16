e17fec472a299139a6cff5802e92684de52703cc
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-10-15 23:40:47 -0300

    Creating Orphanage component and CSS

--- web/src/pages/OrphanagesMaps.tsx
+++ web/src/pages/OrphanagesMaps.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 export default function OrphanagesMap(){
     return (
-        <h1>Hello World</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+                    <h2>Choose an Orphanage on the Map</h2>
+                    <p>Children are waiting for your visit</p>
+                </header>
+
+                <footer>
+                    <strong>São Bernardo do Campo</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
     )
 }
